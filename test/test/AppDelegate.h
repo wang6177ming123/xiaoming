0b8b1b718217ec51b9aaece33f86978f2fa19336
MatthewLuo1 <yang.luo1@pactera.com>
2017-02-14 16:54:28 +0800

    hitTest:event
    hittest:event demo展示

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by User on 15/1/12.
+//  Copyright (c) 2015年 wzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
